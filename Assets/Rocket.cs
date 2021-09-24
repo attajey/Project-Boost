c558257f88ab68819770eb13190420cef9bb0135
Atta Jirofty <jirofty76@gmail.com>
2021-09-24 11:47:50 +0330

    Thrusting Added to Rocket
    By using Space bar, A key & D key, you can somehow move the rocket.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProccesInput();
+        
+    }
+
+    private void ProccesInput()
+    {
+        if (Input.GetKey(KeyCode.Space)) //can thrust while roatating
+        {
+            print("thrusting");
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Left");
+            rigidBody.AddRelativeForce(Vector3.left);
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("right");
+            rigidBody.AddRelativeForce(Vector3.right);
+
+        }
+
+
+    }
+}
